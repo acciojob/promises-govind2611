373496d67d4fc9e2a1cf5b802fef67a22ea67e3e
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-09-30 07:55:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,83 @@
-//your JS code here. If required.
+class MyPromise {
+  constructor(executor) {
+    this.state = 'pending';
+    this.value = null;
+    this.fulfilledCallbacks = [];
+    this.rejectedCallbacks = [];
+
+    const resolve = (value) => {
+      if (this.state === 'pending') {
+        this.state = 'fulfilled';
+        this.value = value;
+        this.executeCallbacks(this.fulfilledCallbacks, value);
+      }
+    };
+
+    const reject = (reason) => {
+      if (this.state === 'pending') {
+        this.state = 'rejected';
+        this.value = reason;
+        this.executeCallbacks(this.rejectedCallbacks, reason);
+      }
+    };
+
+    try {
+      executor(resolve, reject);
+    } catch (error) {
+      reject(error);
+    }
+  }
+
+  then(onFulfilled, onRejected) {
+    return new MyPromise((resolve, reject) => {
+      const fulfilledHandler = (value) => {
+        try {
+          if (typeof onFulfilled === 'function') {
+            const result = onFulfilled(value);
+            resolve(result);
+          } else {
+            resolve(value);
+          }
+        } catch (error) {
+          reject(error);
+        }
+      };
+
+      const rejectedHandler = (reason) => {
+        try {
+          if (typeof onRejected === 'function') {
+            const result = onRejected(reason);
+            resolve(result);
+          } else {
+            reject(reason);
+          }
+        } catch (error) {
+          reject(error);
+        }
+      };
+
+      if (this.state === 'fulfilled') {
+        fulfilledHandler(this.value);
+      } else if (this.state === 'rejected') {
+        rejectedHandler(this.value);
+      } else {
+        this.fulfilledCallbacks.push(fulfilledHandler);
+        this.rejectedCallbacks.push(rejectedHandler);
+      }
+    });
+  }
+
+  catch(onRejected) {
+    return this.then(null, onRejected);
+  }
+
+  executeCallbacks(callbacks, value) {
+    setTimeout(() => {
+      callbacks.forEach((callback) => {
+        callback(value);
+      });
+    }, 0);
+  }
+}
+
+module.exports = MyPromise;
